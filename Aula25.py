aa3a66465f5d3b8adc41904382bad536a27e4fc9
TiagoFrontEnd-Dev <tiago.engsoft@outlook.com>
2024-01-30 13:10:56 -0300

    Aulas_Python_Udemy

---
+++ Aula25.py
@@ -0,0 +1,25 @@
+"""
+Interpolação básica de strings
+s - string
+d e i - int
+f - float
+x e X - Hexadecimal (ABCDEF0123456789)
+"""
+
+# nome = 'Luiz'
+# preco = 1000.95897643
+# variavel = '%s, o preço é R$%.2f' % (nome, preco)
+# print(variavel)
+# print('O hexadecimal de %d é %08X' % (1500, 1500))
+
+
+
+
+
+nome = 'Tiago'
+produto = 'Relogio'
+preco = 250.99999999
+
+variavel = '%s, Seu %s esta no proço de R$ %.2f' % (nome, produto, preco)
+print(variavel)
+
