6f45af2860ba186f1ea450570df3c4444d1aae50
TiagoFrontEnd-Dev <tiago.engsoft@outlook.com>
2024-01-31 12:35:01 -0300

    Aulas_Python_Udemy

---
+++ Aula35.py
@@ -0,0 +1,12 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+contador = 10
+
+###
+
+contador *= '2'
+print(contador)
